dd9fec394b78b7b87f28490b065f8f8762602d80
Matt Griswold <grizz@20c.com>
2018-08-16 14:23:46 +0000

    add Config

---
+++ confu/config.py
@@ -0,0 +1,64 @@
+from __future__ import (absolute_import, division, print_function)
+
+import collections
+import copy
+
+import confu.schema
+
+
+class Config(collections.Mapping):
+    """
+    class for storing and manipulating config data
+    """
+
+    def __init__(self, schema, data=None, meta=None):
+        """
+        `schema` confu.schema object
+        `data` dict to set initial data
+        `meta` any addition metadata to pass along with config
+        """
+        self._base_data = None
+        self._data = None
+        self._schema = schema
+        self.meta = meta if meta else {}
+
+        self.errors = []
+        self.warnings = []
+
+        # use property setter to apply
+        self.data = data if data else {}
+
+    def copy(self):
+        """ return a read only copy of data """
+        return copy.deepcopy(self.data)
+
+    @property
+    def data(self):
+        """ config data, should be used for read only """
+        if self._data:
+            return self._data
+
+        data = copy.deepcopy(self._base_data)
+        confu.schema.apply_defaults(self._schema, data)
+        valid, self.errors, self.warnings = confu.schema.validate(self.schema, data)
+        self._data = data
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        self._base_data = value
+        self._data = None
+
+    @property
+    def schema(self):
+        """ return a read only copy of schema """
+        return copy.deepcopy(self._schema)
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __len__(self):
+        return len(self._data)
---
+++ tests/test_config.py
@@ -0,0 +1,31 @@
+
+import confu
+from confu.config import Config
+from .schemas import Schema_04
+
+
+def test_config_init():
+    cfg = Config(Schema_04())
+
+    # check internal cache
+    data = cfg.data
+    assert data == cfg.data
+
+
+def test_config_copy():
+    cfg = Config(Schema_04())
+    cp = cfg.copy()
+    assert cfg.data == cp
+
+    cp["updated"] = True
+    assert cfg.data != cp
+
+
+def test_config_mapping():
+    cfg = Config(Schema_04())
+    cp = cfg.copy()
+    assert cfg.data == cp
+    assert len(cfg) == len(cp)
+
+    for key in cfg:
+        assert cp[key] == cfg[key]
