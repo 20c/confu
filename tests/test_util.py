8baeebd22a694c7d14163d4da394c6b49d11eb77
Elliot Frank <elliot@20c.com>
2021-02-19 12:34:34 -0600

    Add setting functions to confu utilities

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,7 +1,13 @@
 import configparser
 import os
+import pytest
 
-from confu.util import config_parser_dict
+from confu.util import (
+    config_parser_dict,
+    _set_option,
+    _set_bool,
+
+)
 
 
 def test_config_parser_dict():
@@ -10,3 +16,112 @@ def test_config_parser_dict():
     config.read(path)
 
     assert config_parser_dict(config) == {"test": {"a": "test"}}
+
+
+def test_set_option():
+    context = {}
+    _set_option("TEST_SETTING", "hello", context)
+    assert context["TEST_SETTING"] == "hello"
+
+
+def test_set_option_w_env_var():
+    """
+    Environment variables take precedence over provided options
+    """
+    context = {}
+    os.environ["TEST_SETTING"] = "world"
+    _set_option("TEST_SETTING", "hello", context)
+    assert context["TEST_SETTING"] == "world"
+
+
+def test_set_option_coerce_env_var():
+    """
+    We coerce the environment variable to the same type
+    as the provided default.
+    """
+    context = {}
+    # env variables can never be set as integers
+    os.environ["TEST_SETTING"] = "123"
+
+    # setting an option with a default integer will coerce the env
+    # variable as well (fix for issue #888)
+    _set_option("TEST_SETTING", 321, context)
+    assert context["TEST_SETTING"] == 123
+
+    # setting an option with a default string will coerce the env
+    # variable as well
+    _set_option("TEST_SETTING", "321", context)
+    assert context["TEST_SETTING"] == "123"
+
+    _set_option("TEST_SETTING", 123.1, context)
+    assert context["TEST_SETTING"] == 123.0
+
+
+def test_set_option_booleans():
+
+    context = {}
+    # env variables can only be set as strings
+    os.environ["TEST_SETTING"] = "False"
+
+    # setting the option with a boolean
+    # will use set_bool to handle the
+    # type coercion of the env variable
+    _set_option("TEST_SETTING", False, context)
+    assert context["TEST_SETTING"] is False
+
+    # the environment variable has precedence
+    _set_option("TEST_SETTING", True, context)
+    assert context["TEST_SETTING"] is False
+
+    del os.environ["TEST_SETTING"]
+    del context["TEST_SETTING"]
+    _set_option("TEST_SETTING", True, context)
+    # We can set boolean values without env vars as well
+    assert context["TEST_SETTING"] is True
+
+
+def test_set_bool():
+    """
+    We coerce the environment variable to a boolean
+    """
+    context = {}
+
+    # 0 is interpreted as False
+    os.environ["TEST_SETTING"] = "0"
+    # env variables can never be set as integers
+    _set_bool("TEST_SETTING", False, context)
+    assert context["TEST_SETTING"] is False
+
+    # the environment variable has precedence
+    _set_bool("TEST_SETTING", True, context)
+    assert context["TEST_SETTING"] is False
+
+    # We raise an error if the env variable
+    # cannot be reasonably coerced to a bool
+
+    os.environ["TEST_SETTING"] = "100"
+    with pytest.raises(ValueError):
+        _set_option("TEST_SETTING", True, context)
+
+
+def test_set_options_none():
+    """
+    We coerce the environment variable to a boolean
+    """
+    context = {}
+
+    # 0 is interpreted as False
+    os.environ["TEST_SETTING"] = "0"
+
+    # setting an option with None without setting the
+    # envvar_type raises an error
+    with pytest.raises(ValueError):
+        _set_option("TEST_SETTING", None, context)
+
+    # setting an option with None but setting the
+    # envvar_type is fine
+    _set_option("TEST_SETTING", None, context, envvar_type=int)
+    assert context["TEST_SETTING"] == 0
+
+    _set_option("TEST_SETTING", None, context, envvar_type=str)
+    assert context["TEST_SETTING"] == "0"
