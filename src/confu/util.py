6f25b8953347314afbaf556f8d6706ffc04d5346
egfrank <elliot@20c.com>
2021-02-27 07:35:17 -0600

    Add settings util functions (#44)
    * Add poetry and update tox to use poetry
    
    * Black and isort files
    
    * add github workflow (flake commented out at the moment)
    
    * Get flake8 passing
    
    * re-enable flake8 on github workflow
    
    * remove travis
    
    * Remove setup.cfg and setup.py
    
    * Add docstring for default_from_schema keyword arg
    
    * Update Changelog
    
    * Update latest versino in pyproject.toml
    
    * Add setting functions to confu utilities
    
    * lint
    
    * lint tests
    
    * add (failing) wrapper tests
    
    * Refactor tests using class
    
    * remove django reference
    
    * mv to src, fix tox and remove cruft
    
    * fix github action poetry install / caching
    
    * try without poetry cache
    
    * comment out poetry caching for now with explanation
    
    Co-authored-by: Stefan Pratter <stefan@20c.com>

---
+++ src/confu/util.py
@@ -0,0 +1,93 @@
+"""
+utility functions / classes
+"""
+import os
+
+
+def config_parser_dict(config):
+    """
+    Takes a configparser.ConfigParser instance and returns a dict
+    of sections and their keys and values
+
+    **Arguments**
+
+    - config (`configparser.ConfigParsers`)
+
+    **Returns**
+
+    dict
+    """
+    return {s: dict(config.items(s)) for s in config.sections()}
+
+
+_DEFAULT_ARG = object()
+
+
+class SettingsManager:
+    def __init__(self, scope):
+        self.scope = scope
+
+    def set_from_env(self, name, default=_DEFAULT_ARG):
+        """
+        Sets a scope variable from a environment variable of the same name.
+
+        This is useful to leave the option unset and use default if it
+        already exists in the scope (which may change).
+        """
+        if default is _DEFAULT_ARG and name not in os.environ:
+            return
+
+        self.scope[name] = os.environ.get(name, default)
+
+    def set_option(self, name, value, envvar_type=None):
+        """
+        Sets an option, first checking for env vars,
+        then checking for value already set,
+        then going to the default value if passed.
+        Environment variables are always strings, but
+        we try to coerce them to the correct type first by checking
+        the type of the default value provided. If the default
+        value is None, then we check the optional envvar_type arg.
+        """
+
+        # If value is in True or False we
+        # call set_bool to take advantage of
+        # its type checking for environment variables
+        if isinstance(value, bool):
+            return self.set_bool(name, value)
+
+        if value is not None:
+            envvar_type = type(value)
+        else:
+            # If value is None, we'll use the provided envvar_type, if it is not None
+            if envvar_type is None:
+                raise ValueError(
+                    f"If no default value is provided for the setting {name} the envvar_type argument must be set."
+                )
+
+        if name in os.environ:
+            env_var = os.environ.get(name)
+            # Coerce type based on provided value
+            self.scope[name] = envvar_type(env_var)
+        # If the environment variable isn't set
+        else:
+            self.set_default(name, value)
+
+    def set_bool(self, name, value):
+        """ Sets and option, first checking for env vars, then checking for value already set, then going to the default value if passed. """
+        if name in os.environ:
+            envval = os.environ.get(name).lower()
+            if envval in ["1", "true", "y", "yes"]:
+                self.scope[name] = True
+            elif envval in ["0", "false", "n", "no"]:
+                self.scope[name] = False
+            else:
+                raise ValueError(
+                    "{} is a boolean, cannot match '{}'".format(name, os.environ[name])
+                )
+        self.set_default(name, value)
+
+    def set_default(self, name, value):
+        """ Sets the default value for the option if it's not already set. """
+        if name not in self.scope:
+            self.scope[name] = value
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,7 +1,28 @@
 import configparser
 import os
 
-from confu.util import config_parser_dict
+import pytest
+
+from confu.util import SettingsManager, config_parser_dict
+
+
+@pytest.fixture()
+def globals_fixture():
+    # Setup
+    g = globals()
+    yield g
+    # Teardown
+    del g["TEST_SETTING"]
+
+
+@pytest.fixture()
+def envvar_fixture():
+    # Setup
+    os.environ["TEST_SETTING"] = "world"
+    yield
+    # Teardown
+    if os.environ.get("TEST_SETTING"):
+        del os.environ["TEST_SETTING"]
 
 
 def test_config_parser_dict():
@@ -10,3 +31,164 @@ def test_config_parser_dict():
     config.read(path)
 
     assert config_parser_dict(config) == {"test": {"a": "test"}}
+
+
+def test_set_option_global(globals_fixture):
+    g = globals_fixture
+    settings = SettingsManager(g)
+    settings.set_option("TEST_SETTING", "world")
+    assert g["TEST_SETTING"] == "world"
+
+
+def test_settings_manager():
+    scope = {}
+    settings = SettingsManager(scope)
+    assert settings.scope == scope
+
+
+def test_set_from_env(envvar_fixture):
+    scope = {}
+    settings = SettingsManager(scope)
+    settings.set_from_env("TEST_SETTING")
+    assert scope["TEST_SETTING"] == "world"
+
+
+def test_set_from_env_no_env_no_default():
+    scope = {}
+    settings = SettingsManager(scope)
+    assert os.environ.get("TEST_SETTING") is None
+    settings.set_from_env("TEST_SETTING")
+    assert scope.get("TEST_SETTING") is None
+
+
+def test_set_from_env_no_env_w_default():
+    scope = {}
+    settings = SettingsManager(scope)
+    assert os.environ.get("TEST_SETTING") is None
+    settings.set_from_env("TEST_SETTING", default="default")
+    assert scope["TEST_SETTING"] == "default"
+
+
+def test_set_option():
+    scope = {}
+    settings = SettingsManager(scope)
+    settings.set_option("TEST_SETTING", "hello")
+    assert scope["TEST_SETTING"] == "hello"
+
+
+def test_set_option_w_env_var(envvar_fixture):
+    """
+    Environment variables take precedence over provided options
+    """
+    scope = {}
+    settings = SettingsManager(scope)
+    settings.set_option("TEST_SETTING", "hello")
+    assert scope["TEST_SETTING"] == "world"
+
+
+def test_set_option_coerce_env_var(envvar_fixture):
+    """
+    We coerce the environment variable to the same type
+    as the provided default.
+    """
+    scope = {}
+    settings = SettingsManager(scope)
+    # env variables can never be set as integers
+    os.environ["TEST_SETTING"] = "123"
+
+    # setting an option with a default integer will coerce the env
+    # variable as well (fix for issue #888)
+
+    settings.set_option("TEST_SETTING", 321)
+    assert scope["TEST_SETTING"] == 123
+
+    # setting an option with a default string will coerce the env
+    # variable as well
+    settings.set_option("TEST_SETTING", "321")
+    assert scope["TEST_SETTING"] == "123"
+
+    settings.set_option("TEST_SETTING", 123.1)
+    assert scope["TEST_SETTING"] == 123.0
+
+
+def test_set_option_booleans(envvar_fixture):
+
+    scope = {}
+    settings = SettingsManager(scope)
+    # env variables can only be set as strings
+    os.environ["TEST_SETTING"] = "False"
+
+    # setting the option with a boolean
+    # will use set_bool to handle the
+    # type coercion of the env variable
+    settings.set_option("TEST_SETTING", False)
+    assert scope["TEST_SETTING"] is False
+
+    # the environment variable has precedence
+    settings.set_option("TEST_SETTING", True)
+    assert scope["TEST_SETTING"] is False
+
+    del os.environ["TEST_SETTING"]
+    del scope["TEST_SETTING"]
+    settings.set_option("TEST_SETTING", True)
+    # We can set boolean values without env vars as well
+    assert scope["TEST_SETTING"] is True
+
+
+def test_set_bool_global(globals_fixture):
+    g = globals_fixture
+    settings = SettingsManager(g)
+
+    assert "TEST_SETTING" not in g
+    settings.set_bool("TEST_SETTING", False)
+    assert g["TEST_SETTING"] is False
+
+
+def test_set_bool(envvar_fixture):
+    """
+    We coerce the environment variable to a boolean
+    """
+    scope = {}
+    settings = SettingsManager(scope)
+    # 0 is interpreted as False
+    os.environ["TEST_SETTING"] = "0"
+    # env variables can never be set as integers
+    settings.set_bool("TEST_SETTING", False)
+    assert scope["TEST_SETTING"] is False
+
+    # the environment variable has precedence
+    settings.set_bool("TEST_SETTING", True)
+    assert scope["TEST_SETTING"] is False
+
+    # We raise an error if the env variable
+    # cannot be reasonably coerced to a bool
+
+    os.environ["TEST_SETTING"] = "100"
+    with pytest.raises(ValueError):
+        settings.set_bool("TEST_SETTING", True)
+
+
+def test_set_options_none(envvar_fixture):
+    """
+    We coerce the environment variable to a boolean
+    """
+    scope = {}
+    settings = SettingsManager(scope)
+    # 0 is interpreted as False
+    os.environ["TEST_SETTING"] = "0"
+
+    # setting an option with None without setting the
+    # envvar_type raises an error
+    with pytest.raises(ValueError):
+        settings.set_option(
+            "TEST_SETTING",
+            None,
+        )
+
+    # setting an option with None but setting the
+    # envvar_type is fine
+    settings.set_option("TEST_SETTING", None, envvar_type=int)
+    assert scope["TEST_SETTING"] == 0
+
+    settings.set_option("TEST_SETTING", None, envvar_type=str)
+    assert scope["TEST_SETTING"] == "0"
