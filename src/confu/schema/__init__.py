536c0ffa24ffeaf8ae86d067265a9bd242f8fa2d
Matt Griswold <grizz@20c.com>
2021-07-17 21:38:39 -0500

    poetry lock and update

--- src/confu/schema/__init__.py
+++ src/confu/schema/__init__.py
@@ -1,2 +1,2 @@
 from confu.schema.core import *  # noqa
-from confu.schema.inet import Email, IpAddress, Url, IpNetwork  # noqa
+from confu.schema.inet import Email, IpAddress, IpNetwork, Url  # noqa
