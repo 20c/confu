6820062bf4d30d3774aa648eb10cfa0e535342fe
Jason Carvalho <jason@20c.com>
2022-03-10 14:07:19 +0530

    linting

--- examples/settingsmanager/example.py
+++ examples/settingsmanager/example.py
@@ -1,8 +1,6 @@
 import os
 
-from confu.util import SettingsManager, config_parser_dict
-
-
+from confu.util import SettingsManager
 
 # using local scope g
 g = {}
@@ -16,28 +14,28 @@ settings_manager.set_option("TEST_SETTING", "world")
 print(TEST_SETTING)
 
 # setting from env
-os.environ["ENV_SETTING"] = "my_setting" #setting env variable
+os.environ["ENV_SETTING"] = "my_setting"  # setting env variable
 settings_manager.set_from_env("ENV_SETTING")
 print(ENV_SETTING)
 
-# setting boolean 
+# setting boolean
 settings_manager.set_bool("BOOL_SETTING", False)
 print(BOOL_SETTING)
 
 # setting boolean overriden from env var from env
-os.environ["ENV_BOOL"] = "True" #setting env variable
+os.environ["ENV_BOOL"] = "True"  # setting env variable
 settings_manager.set_bool("ENV_BOOL", False)
 print(ENV_BOOL)
 
 # setting int from env
-os.environ["ENV_INT"] = "123" #setting env variable
+os.environ["ENV_INT"] = "123"  # setting env variable
 settings_manager.set_option("ENV_INT", None, envvar_type=int)
 print(ENV_INT)
 
 # setting default
-settings_manager.set_default("DEFAULT_SETTING", 'my_defualt')
+settings_manager.set_default("DEFAULT_SETTING", "my_defualt")
 print(DEFAULT_SETTING)
 
 # include another file (./test.py)
-settings_manager.try_include('./test.py')
+settings_manager.try_include("./test.py")
 print(EXTERNAL_SETTING)
