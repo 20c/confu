5f6bf9161d640fcbaecbddec93f8be1c6b20b051
Jason Carvalho <jason@20c.com>
2022-04-21 18:51:10 +0530

    linting

--- src/confu/schema/core.py
+++ src/confu/schema/core.py
@@ -352,6 +352,7 @@ class TimeDuration(Attribute):
                 raise ValueError("unknown unit from interval string '%s'" % val)
         return super().validate(total, path, **kwargs)
 
+
 class Int(Attribute):
 
     """
