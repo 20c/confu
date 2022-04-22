f76218b5b155b6aa6fad6840e4b15f53573502f3
Jason Carvalho <jason@20c.com>
2022-04-22 16:35:01 +0530

    add functionality convert to float in TimeDuration

--- src/confu/types.py
+++ src/confu/types.py
@@ -13,31 +13,36 @@ class TimeDuration(float):
         90 = 1m30s
     units accepted are d(days), h(hours), m(minutes),
     s(seconds) and ms(milliseconds).
+    Can also take a `float`, `int` or string without 
+    unit instead.
 
     **Arguments**
 
-    - val (`str`)
+    - val (`str`, `int` or `float`)
     """
 
     def __new__(cls, value, **kwargs):
-        re_intv = re.compile(r"([\d\.]+)([a-zA-Z]+)")
-        re_validate = re.compile(r"(([\d\.]+)([s,m,ms,h,d]+))*")
-        formatted_val = value.replace(" ", "")
+        try:
+            total = float(value)
+        except ValueError:
+            re_intv = re.compile(r"([\d\.]+)([a-zA-Z]+)")
+            re_validate = re.compile(r"(([\d\.]+)([s,m,ms,h,d]+))*")
+            formatted_val = value.replace(" ", "")
 
-        total = 0.0
-        if not re.fullmatch(re_validate, formatted_val):
-            raise ValueError("unknown unit or format in interval string '%s'" % value)
-        for match in re_intv.findall(formatted_val):
-            unit = match[1]
-            count = float(match[0])
-            if unit == "s":
-                total += count
-            elif unit == "m":
-                total += count * 60
-            elif unit == "ms":
-                total += count / 1000
-            elif unit == "h":
-                total += count * 3600
-            elif unit == "d":
-                total += count * 86400
+            total = 0.0
+            if not re.fullmatch(re_validate, formatted_val):
+                raise ValueError("unknown unit or format in interval string '%s'" % value)
+            for match in re_intv.findall(formatted_val):
+                unit = match[1]
+                count = float(match[0])
+                if unit == "s":
+                    total += count
+                elif unit == "m":
+                    total += count * 60
+                elif unit == "ms":
+                    total += count / 1000
+                elif unit == "h":
+                    total += count * 3600
+                elif unit == "d":
+                    total += count * 86400
         return super(TimeDuration, cls).__new__(cls, total)
