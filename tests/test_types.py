c0fbcf18d2cae890fa68e89890f7c137a00036e1
Jason Carvalho <jason@20c.com>
2022-04-22 16:37:56 +0530

    update tests for test_TimeDuration

--- tests/test_types.py
+++ tests/test_types.py
@@ -5,11 +5,12 @@ from confu.types import TimeDuration
 
 def test_TimeDuration():
     assert 180122.002 == TimeDuration("2d 2h 2m 2s 2ms")
+    assert 180122.002 == TimeDuration("180122.002")
+    assert 180122.002 == TimeDuration(180122.002)
+    assert 180122 == TimeDuration(180122)
 
     with pytest.raises(ValueError):
         TimeDuration("2x 2h 2m 2s 2ms")
-    with pytest.raises(ValueError):
-        TimeDuration("2")
     with pytest.raises(ValueError):
         TimeDuration("s")
     with pytest.raises(ValueError):
